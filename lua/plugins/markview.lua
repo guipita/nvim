3a0e1ced9687c1422ad7ed73a7b2b44cd995aac3
Guilherme Pita <guilherme.pita@gmail.com>
2025-08-25 22:22:14 -0400

    ensure markview is loaded after treesitter

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,6 +1,17 @@
--- For `plugins/markview.lua` users.
 return {
-  "OXY2DEV/markview.nvim",
-  lazy = false,
-  experimental = { check_rtp_message = false },
+  -- Ensure treesitter has a higher priority as a start plugin
+  -- in order for its rtp to effectively have a lower priority.
+  -- 50 is lazy's default priority.
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    priority = 50,
+  },
+  {
+    "OXY2DEV/markview.nvim",
+    -- ensure markview is treated as a start plugin
+    lazy = false,
+    -- and is loaded after nvim-treesitter
+    priority = 49,
+  },
 }
