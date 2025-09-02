af90a0fe5d53e927b50e34be9e239ea532a9dc34
Guilherme Pita <guilherme.pita@gmail.com>
2025-09-02 00:06:39 -0400

    update settings to latest recommended

--- lua/plugins/python-venv.lua
+++ lua/plugins/python-venv.lua
@@ -3,44 +3,15 @@ return {
   dependencies = {
     "neovim/nvim-lspconfig",
     "mfussenegger/nvim-dap",
-    "mfussenegger/nvim-dap-python",
+    "mfussenegger/nvim-dap-python", --optional
     { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
   },
-  lazy = false,
-  branch = "main", -- This is the regexp branch, use this for the new version
-  config = function()
-    local function on_venv_activate()
-      local command_run = false
-
-      local function run_shell_command()
-        local venv = require("venv-selector").venv()
-
-        if command_run == false then
-          local command = "source " .. venv .. "/bin/activate"
-          vim.api.nvim_feedkeys(command .. "\n", "n", false)
-          command_run = true
-        end
-      end
-
-      vim.api.nvim_create_augroup("TerminalCommands", { clear = true })
-
-      vim.api.nvim_create_autocmd("TermEnter", {
-        group = "TerminalCommands",
-        pattern = "*",
-        callback = run_shell_command,
-      })
-    end
-
-    require("venv-selector").setup({
-      settings = {
-        options = {
-          on_venv_activate_callback = on_venv_activate,
-        },
-      },
-    })
-  end,
+  ft = "python", -- Load when opening Python files
   keys = {
-    -- Keymap to open VenvSelector to pick a venv.
-    { "<leader>v", "<cmd>VenvSelect<cr>" },
+    { "<leader>v", "<cmd>VenvSelect<cr>" }, -- Open picker on keymap
+  },
+  opts = { -- this can be an empty lua table - just showing below for clarity.
+    search = {}, -- if you add your own searches, they go here.
+    options = {}, -- if you add plugin options, they go here.
   },
 }
